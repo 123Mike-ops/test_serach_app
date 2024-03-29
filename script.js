4a3d46177a5d382cfd7a4c8f711785be3ab7744e
unknown <mikiyasdaniel983@gmail.com>
2024-03-29 09:34:22 +0300

    done

--- script.js
+++ script.js
@@ -15,11 +15,15 @@ document.getElementById("searchInput").addEventListener("input", function() {
 });
 
 function displayResults(results) {
+    let input = document.getElementById("searchInput").value.trim().toLowerCase();
     let resultsContainer = document.getElementById("results");
-    resultsContainer.innerHTML = "";
-    results.forEach(function(result) {
-        let resultDiv = document.createElement("div");
-        resultDiv.textContent = result.name;
-        resultsContainer.appendChild(resultDiv);
+    resultsContainer.innerHTML = ""; 
+    results.forEach(function(comment) {
+
+        if (comment.body.toLowerCase().includes(input) || comment.name.toLowerCase().includes(input)) {
+            let resultDiv = document.createElement("div");
+            resultDiv.textContent = comment.body;
+            resultsContainer.appendChild(resultDiv);
+        }
     });
 }
